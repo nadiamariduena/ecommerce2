ee807607ee1b6218a3b3609079d823711697811e
melissa <nadia.mm.mariduena@gmail.com>
2020-11-10 20:48:39 +0100

    category , subcategory and branche of subcategory done! | push an additional object for any new subcategory

--- README.md
+++ README.md
@@ -3225,3 +3225,73 @@ exports.adminMiddleware = (req, res, next) => {
 - DONT use the same CATEGORY name for a SUB category
 
 [<img src="./src/img/creating-subcategory-idparent.jpg">](https://www.youtube.com/watch?v=yLlgVCLhuN4)
+
+- So in such way you can add several subcategories to the categories
+
+- result
+
+![rested](./src/img/check-subcategory.jpg)
+
+- YOU CAN CREATE BRANCHES INSIDE THE SUBCATEGORY BY USING THE "SUBCATEGORY \_ID" and PROCEDING LIKE YOU DID "TO CREATE A SUBCATEGORY" (CHECK THE VIDEO 1 img above)
+
+<br>
+
+#### NOW CREATE A FUNCTION TO FETCH THE SUBCATEGORIES :camel:
+
+- CREATE A RECURSIVE FUNCTION
+
+##### what is a recursive function?
+
+<p>Recursive function is a fucntion that calls itself. WHEN writing recursive fucntions there must be a definite BREK POINT condition, otherwise we risk creating infinite loops</p>
+
+<br>
+
+##### GO TO THE categories.js CONTROLLER
+
+- - inside the postman
+
+[<img src="./src/img/category_subcategory.jpg">](https://youtu.be/3LMvCXZwnzc)
+
+<br>
+
+##### HERE YOU CAN NOTICE the name change in the arrays containing the categories
+
+- before it was categories[] now it s categoryList[]
+
+```javascript
+/*
+PUSHING THE SUBCATEGORY OBJECT TO THE CATEGORY
+
+  for (let cate of category) {
+    // it will push an object
+    categoryList.push({
+      _id_: cate._id,
+      name: cate.name,
+      slug: cate.slug,
+      // here below is the recursive call, in the call you are passing the complete categories
+      // which is "categories" , then to filter the result you are going to pass the id of the current id  (categories, cate._id),
+      children: createCategories(categories, cate._id),
+    });
+  }
+
+
+*/
+{
+    "categoryList": [
+// THIS IS THE OBJECT PUSHED TO THE CATEGORY
+        {
+            "_id_": "5fa9b5df5622f8022c1708b3",
+            "name": "plantitas",
+            "slug": "plantitas",
+            "children": [
+                {
+                    "_id_": "5faab6509f6c3a18add8588a",
+                    "name": "indoor plant",
+                    "slug": "indoor-plant",
+                    "children": []
+                }
+            ]
+        }
+    ]
+}
+```
--- src/controller/category.js
+++ src/controller/category.js
@@ -1,6 +1,54 @@
 const Category = require("../models/category");
 const slugify = require("slugify");
 
+// -------------------
+//  THIS is related to the :
+// -----------------
+// C A T E G O R Y list  ********* check on the bottom
+//
+// R E C UR S I V E function related to the branches of the subcategories
+
+function createCategories(categories, parentId = null) {
+  const categoryList = [];
+  let category;
+
+  if (parentId == null) {
+    // here its going to fetch all the parent categories
+    category = categories.filter((cat) => cat.parentId == undefined);
+  } else {
+    // it will fetch data based on the ID
+    category = categories.filter((cat) => cat.parentId == parentId);
+  }
+  // ---
+  for (let cate of category) {
+    // it will push an object
+    categoryList.push({
+      _id_: cate._id,
+      name: cate.name,
+      slug: cate.slug,
+      // here below is the recursive call, in the call you are passing the complete categories
+      // which is "categories" , then to filter the result you are going to pass the id of the current id  (categories, cate._id),
+      children: createCategories(categories, cate._id),
+    });
+  }
+  // ---
+  return categoryList;
+  // dont forget to return it otherwise you will see an empty arrow inside the postman when testing it
+}
+// ------------------
+// ------------------
+/*
+
+
+
+
+
+
+
+*/
+// ------------------
+// A D D  category
+// ------------------
 exports.addCategory = (req, res) => {
   const categoryObj = {
     name: req.body.name,
@@ -16,8 +64,8 @@ exports.addCategory = (req, res) => {
     if (error) return res.status(400).json({ error });
 
     if (category) {
-      // if there is no error, it will add the category you added
       return res.status(201).json({ category });
+      // if there is no error, it will add the category you added
       //   201 success
     }
   });
@@ -36,7 +84,17 @@ exports.getCategories = (req, res) => {
     //
     //
     if (categories) {
-      res.status(200).json({ categories });
+      // -----------------
+      // C A T E G O R Y list  *********
+      // AFTER ADDING THE SUB-CATEGORIES
+      // WRITE the recursive function here:
+      const categoryList = createCategories(categories);
+      // -----------------
+      // this was replaced by categoriesList for the recursive function related to the
+      //  branches of the subcategories, that is on top of this file.
+      res.status(200).json({ categoryList });
+      // before caegoriesList it was categories
+      // res.status(200).json({ categories });
     }
   });
 };
